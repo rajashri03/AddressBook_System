c4ed213de460aee3102f100ccbeede93dbd80347
rajashri03 <telvekar.rajashri@gmail.com>
2021-12-31 14:43:23 +0530

    [Rajashri]Add:Create a contact in address book

---
+++ AddressBook/AddressBook/AddressBookEntry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class AddressBookEntry
+    {
+        //Created list of contact
+        List<Contact> list = new List<Contact>();
+        /// <summary>
+        /// Created Contact in address book
+        /// </summary>
+        /// <returns></returns>
+        public Contact CreateContact()
+        {
+            Contact con = new Contact();
+            Console.WriteLine("***************Address Book********************");
+            Console.WriteLine("Enter First Name");
+            con.Firstname = Console.ReadLine();
+            Console.WriteLine("Enter Last name");
+            con.LastName = Console.ReadLine();
+            Console.WriteLine("Enter age");
+            con.Age = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter PhoneNumber");
+            con.Phonenumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter Address");
+            con.Address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            con.City = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            con.State = Console.ReadLine();
+            Console.WriteLine("Enter Email");
+            con.Email = Console.ReadLine();
+            return con;
+        }
+    }
+}
---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AddressBook;
+
+AddressBookEntry entry = new AddressBookEntry();
+Contact con = entry.CreateContact();
+
+Console.WriteLine("**********Contact in address Book*************");
+Console.WriteLine($"First Name: {con.Firstname}");
+Console.WriteLine($"Last Name: {con.LastName}");
+Console.WriteLine($"Age: {con.Age}");
+Console.WriteLine($"Phone Number: {con.Phonenumber}");
+Console.WriteLine($"Address: {con.Address}");
+Console.WriteLine($"City: {con.City}");
+Console.WriteLine($"State: {con.State}");
+Console.WriteLine($"Email: {con.Email}");
